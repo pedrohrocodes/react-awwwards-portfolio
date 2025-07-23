9671ba90b651c83673a7f313f12b497d8701995d
Pedro Oliveira <pedro.hrochaoliveira@gmail.com>
2025-07-23 07:51:25 -0300

    Add GSAP animations to Navbar and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div className='relative w-screen min-h-screen overflow-x-auto'>
       <Navbar />
+      <section id='home' className='min-h-screen'/>
+      <section id='services' className='min-h-screen bg-amber-500'/>
     </div>
   )
 }
