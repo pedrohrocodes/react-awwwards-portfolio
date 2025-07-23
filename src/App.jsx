22f1bf5236d81bb58840625d96a26a7c1331f14c
Pedro Oliveira <pedro.hrochaoliveira@gmail.com>
2025-07-23 10:30:24 -0300

    feat: Add ServiceSummary component with GSAP animations for scroll effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Navbar from './sections/Navbar'
-import Hero from './sections/Hero'
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import ServiceSummary from "./sections/ServiceSummary";
 
 const App = () => {
   return (
-    <div className='relative w-screen min-h-screen overflow-x-auto'>
+    <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
       <Hero />
+      <ServiceSummary />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
